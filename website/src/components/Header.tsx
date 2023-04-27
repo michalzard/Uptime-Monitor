0774a0291f1a55d7d7d039e62363e1571f4df1fe
MichalZard <miso.platko@gmail.com>
2023-04-27 03:44:37 +0200

    smooth animation

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -19,7 +19,7 @@ function Header() {
 
     }, []);
     return (
-        <header className="border-b-2 h-16 p-5 md:px-10 flex items-center justify-between z-50 ">
+        <header className="border-b-2 h-16 p-5 md:px-10 flex items-center bg-white justify-between z-50 sticky top-0 ">
             <nav onClick={() => setSelected(-1)}>
                 <CompanyLogo />
             </nav>
@@ -32,7 +32,7 @@ function Header() {
                             <button onClick={() => { navigate(btn.redirect); setSelected(i) }} className={`text-gray-500 w-28 font-semibold px-3 ${i === buttons.length - 1 ? "mr-4" : ""}
                              transition
                             `}>{btn.name}</button>
-                            <div className={`border-b-2 relative top-[20px] ${i === selected ?
+                            <div className={`border-b-2 relative top-[20px] w-28 ${i === selected ?
                                 `border-b-blue-700 transform transition-all translate-x-0  duration-200 ease-in` :
                                 `border-b-transparent transition-all transform  ${i < selected ? "translate-x-full" : "-translate-x-full"} duration-200 ease-in`} `} />
                         </section>
@@ -42,12 +42,12 @@ function Header() {
 
                 {
                     user.isLoggedIn ?
-                    // using selected 10 so its higher number than amount of buttons in order for transition to go to right side
+                        // using selected 10 so its higher number than amount of buttons in order for transition to go to right side
                         <button onClick={() => { setSelected(10); navigate("/dashboard") }} className="rounded-2xl bg-blue-700 text-white font-semibold px-4 py-1">Dashboard</button>
                         :
                         <>
-                            <button onClick={() => navigate("/signup")} className="mr-2 rounded-2xl bg-blue-700 text-white font-semibold px-4 py-1">Start for free</button>
-                            <button onClick={() => navigate("/signin")} className="rounded-2xl border border-blue-700 text-blue-700 font-semibold px-4 py-1">Sign in</button>
+                            <button onClick={() => { setSelected(10); navigate("/signup") }} className="mr-2 rounded-2xl bg-blue-700 text-white font-semibold px-4 py-1">Start for free</button>
+                            <button onClick={() => { setSelected(10); navigate("/signin") }} className="rounded-2xl border border-blue-700 text-blue-700 font-semibold px-4 py-1">Sign in</button>
                         </>
                 }
             </nav >
