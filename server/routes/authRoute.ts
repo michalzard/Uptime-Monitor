90ff881e47538b7f117f289a3be078b3560d9832
MichalZard <miso.platko@gmail.com>
2023-04-28 03:05:21 +0200

    google endpoint

--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { userGithubAccess, userLogin, userLogout, userRegistration, userSession } from "../controllers/authController";
+import { userLogin, userLogout, userRegistration, userSession } from "../controllers/authController";
+import { userGithubAccess, userGoogleAccess, userGoogleRequestLink } from "../controllers/oauthController";
 const router = express.Router();
 
 // POST register
@@ -16,5 +17,7 @@ router.post("/login", userLogin);
 router.get("/session", userSession);
 router.post("/logout", userLogout);
 router.post("/github", userGithubAccess);
+router.get("/google/url", userGoogleRequestLink);
+router.post("/google/access", userGoogleAccess);
 
 export default router;
