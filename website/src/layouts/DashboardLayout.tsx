2fed18559233e70125723c1847c3eb6f39ab220b
MichalZard <miso.platko@gmail.com>
2023-05-31 21:09:04 +0200

    loading pages from layout so there's no extra function calls

--- website/src/layouts/DashboardLayout.tsx
+++ website/src/layouts/DashboardLayout.tsx
@@ -3,7 +3,6 @@ import Sidebar from "../components/dashboard/Sidebar";
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 import { usePageStore } from "../store/pageStore";
 import { SwipeableDrawer } from "../components/customUI/Drawer";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 function Dashboard() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -11,12 +10,9 @@ function Dashboard() {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  useEffect(() => {
-    if (location.pathname === "/dashboard" && page.currentPage?.id) { navigate(`/dashboard/${page.currentPage?.id}/incidents`); }
-  }, [location]);
 
   // load pages
-  useEffect(() => page.loadAll(navigate), []);
+  useEffect(() => page.loadAll(location, navigate), []);
 
   return (
     <main className="flex w-full h-[calc(100vh-64px)]">
@@ -27,7 +23,7 @@ function Dashboard() {
       </SwipeableDrawer>
       <Outlet />
       {page.pages.length === 0 && location.pathname === "/dashboard" ?
-        <section className="w-full h-full flex flex-col items-center justify-center bg-center bg-no-repeat bg-contain"
+        <section className="w-full lg:w-[calc(100vw-300px)] flex flex-col items-center justify-center bg-center bg-no-repeat bg-contain"
           style={{ backgroundImage: `url(/dashboard/createpage.png)` }} >
           <span className="text-white font-semibold text-3xl lg:text-4xl">Start by creating new page </span>
           {/* <ArrowLeftIcon className="w-8 h-8 lg:w-10 lg:h-10 text-white" /> */}
