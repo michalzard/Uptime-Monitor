442d5a377429533590a36c8f446df146f7e27cfc
MichalZard <miso.platko@gmail.com>
2023-06-05 21:21:38 +0200

    changed config to be slighty faster on build times if i want to preview the application

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  build:{
-    rollupOptions:{
-      experimentalCacheExpiry: 60000, //cache expiry set to 1 min
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,// allows parallel chunking for faster builds
+      },
     }
   }
 })
